0b26301641abfa1d6cabaf4838249762135fe5a1
aliceandherdolls <alicjagorkiewicz92@gmail.com>
2022-07-18 13:20:06 +0200

    function

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 let num1 = 2;
 let num2 = 5;
 
-function action(par1,par2) {
-    let compare= par1<par2;
+function action(par1, par2) {
 
-    if(par1 < par2){
-        console.log(par2)
+    if (par1 < par2) {
+        return num2;
     } else {
-        console.log(par1)
+        return num1;
     }
 }
 
-action(2,5)
+console.log(action(2, 5));
+
 
